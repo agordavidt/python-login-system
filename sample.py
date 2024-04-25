7cf28da31fa4cb0714cae3a9e006c67b8c4383da
David Agor <agordavid14@gmail.com>
2024-04-25 16:13:14 +0100

    sqlite file

--- sample.py
+++ sample.py
@@ -1,1 +1,26 @@
-print("Hello World")
+import sqlite3
+import hashlib
+
+conn = sqlite3.connect("userdata.db")
+cur = conn.cursor()
+
+cur.execute("""
+CREATE TABLE IF NOT EXISTS userdata (
+            id INTEGER PRIMARY KEY,
+            username VARCHAR(255) NOT NULL,
+            password VARCHAR(255) NOT NULL
+)
+            """)
+
+username1, password1 = "john44", hashlib.sha256("johnpassword".encode()).hexdigest()
+username2, password2 = "julie09", hashlib.sha256("juju09".encode()).hexdigest()
+username3, password3 = "mikedeen", hashlib.sha256("mikyky".encode()).hexdigest()
+
+
+cur.execute("INSERT INTO userdata (username, password) VALUES (?, ?)", (username1, password1))
+cur.execute("INSERT INTO userdata (username, password) VALUES (?, ?)", (username2, password2))
+cur.execute("INSERT INTO userdata (username, password) VALUES (?, ?)", (username3, password3))
+
+
+conn.commit()
+
---
+++ server.py
@@ -0,0 +1,12 @@
+import sqlite3
+import hashlib
+import socket
+import threading
+
+server = socket.scocket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("localhost", 9999))
+
+server.listen()
+
+def handle_connection(c):
+    c.send("Username: ")
